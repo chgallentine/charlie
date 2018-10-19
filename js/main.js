0ddbfec3d97196bd0a1956cabb0f6b8576b75846
Charlie Gallentine <chgallentine@gmail.com>
2018-10-18 22:08:30 -0400

    Added name, need resume, social, projects

--- js/main.js
+++ js/main.js
@@ -2,19 +2,23 @@
 * @Author: Charlie Gallentine
 * @Date:   2018-10-09 08:24:59
 * @Last Modified by:   Charlie Gallentine
-* @Last Modified time: 2018-10-09 11:24:40
+* @Last Modified time: 2018-10-18 22:07:20
 */
 
 function adjustBackground()
 {
-	var h = 
+	const h = 
 		window.innerHeight 
 		|| document.documentElement.clientHeight 
 		|| document.body.clientHeight;
 
-	var offset = h - 425;
-	var nav_offset = h - 50;
+	const offset = h - 425;
+	const nav_offset = h - 50;
+	const name_offset = h - (h>500 ? 485 : h > 400 ? 400 : 300);
 
-	document.getElementById("body").style.backgroundPosition = `0 ${offset}px`;
-	document.getElementById("navbar").style.marginTop = `${nav_offset}px`;
+	console.log(h);
+
+	document.getElementsByTagName("body")[0].style.backgroundPosition = `0 ${offset}px`;
+	document.getElementById("navbar").style.top = `${nav_offset}px`;
+	document.getElementById("name").style.top = `${name_offset}px`;
 }
